8b511244c1cd3535386a8bc26635c21b2233f217
Abner Carleto <abner.carleto@ymail.com>
2018-03-11 13:23:41 -0300

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Typography variant="display4" gutterBotton>
+        React WARS
+      </Typography>
     );
   }
 }
